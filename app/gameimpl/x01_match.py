355bf31a4bf9d392ad0f8ac069e9453fb82eda05
Jason Tessier <jason.tessier@gmail.com>
2021-11-07 22:34:39 +0000

    Lab02 - complete with exercises

--- app/gameimpl/x01_match.py
+++ app/gameimpl/x01_match.py
@@ -2,25 +2,197 @@ from service.match_service import MatchVisitTemplate
 from service.match_service import MatchManager
 from datatype.enums import DartMultiplier
 
+# CHECKOUTS = {
+#     170: "T20 T20 Bull",
+#     167: "T20 T19 Bull",
+#     164: "T20 T18 Bull",
+#     161: "T20 T17 Bull",
+#     160: "T20 T20 D20",
+#
+#     136: "T20 T20 D8",
+#
+#     36: "D18"
+# }
+
 CHECKOUTS = {
-    170: "T20 T20 Bull",
-    167: "T20 T19 Bull",
-    164: "T20 T18 Bull",
-    161: "T20 T17 Bull",
+    2: "D1",
+    3: "1 D1",
+    4: "D2",
+    5: "3 D1",
+    6: "D3",
+    7: "5 D1",
+    8: "D4",
+    9: "5 D2",
+    10: "D5",
+    11: "3 D4",
+    12: "D6",
+    13: "3 D5",
+    14: "D7",
+    15: "3 D6",
+    16: "D8",
+    17: "3 D7",
+    18: "D9",
+    19: "3 D8",
+    20: "D10",
+    21: "1 D10",
+    22: "D11",
+    23: "5 D9",
+    24: "D12",
+    25: "3 D11",
+    26: "D13",
+    27: "3 D12",
+    28: "D14",
+    29: "1 D14",
+    30: "D15",
+    31: "3 D14",
+    32: "D16",
+    33: "3 D15",
+    34: "D17",
+    35: "7 D14",
+    36: "D18",
+    37: "5 D16",
+    38: "D19",
+    39: "1 D19",
+    40: "D20",
+    41: "3 D19",
+    42: "4 D19",
+    43: "5 D19",
+    44: "6 D19",
+    45: "7 D19",
+    46: "8 D19",
+    47: "9 D19",
+    48: "10 D19",
+    49: "11 D19",
+    50: "12 D19",
+    51: "13 D19",
+    52: "12 D20",
+    53: "15 D19",
+    54: "14 D20",
+    55: "15 D20",
+    56: "16 D20",
+    57: "17 D20",
+    58: "18 D20",
+    59: "19 D20",
+    60: "20 D20",
+    61: "3 20 D19",
+    62: "16 8 D19",
+    63: "14 9 D20",
+    64: "9 15 D20",
+    65: "7 18 D20",
+    66: "6 20 D20",
+    67: "13 16 D19",
+    68: "11 19 D19",
+    69: "10 19 D20",
+    70: "10 20 D20",
+    71: "14 19 D19",
+    72: "18 16 D19",
+    73: "17 18 D19",
+    74: "16 18 D20",
+    75: "15 20 D20",
+    76: "20 16 D20",
+    77: "17 20 D20",
+    78: "18 20 D20",
+    79: "19 20 D20",
+    80: "20 20 D20",
+    81: "D19 3 D20",
+    82: "D19 4 D20",
+    83: "5 D20 D19",
+    84: "6 D20 D19",
+    85: "7 D19 D20",
+    86: "D20 8 D19",
+    87: "9 D19 D20",
+    88: "D20 10 D19",
+    89: "11 D19 D20",
+    90: "12 D19 D20",
+    91: "13 D19 D20",
+    92: "D19 14 D20",
+    93: "15 D19 D20",
+    94: "16 D20 D19",
+    95: "17 D20 D19",
+    96: "D20 18 D19",
+    97: "19 D20 D19",
+    98: "D20 20 D19",
+    99: "19 D20 D20",
+    100: "20 D20 D20",
+    101: "6 T19 D19",
+    102: "D20 D12 D19",
+    103: "5 T20 D19",
+    104: "9 T19 D19",
+    105: "T20 5 D20",
+    106: "D14 D19 D20",
+    107: "T19 12 D19",
+    108: "D19 D20 D15",
+    109: "T20 9 D20",
+    110: "T19 13 D20",
+    111: "16 T19 D19",
+    112: "D19 D20 D17",
+    113: "T19 18 D19",
+    114: "T19 19 D19",
+    115: "T19 18 D20",
+    116: "16 T20 D20",
+    117: "19 T20 D19",
+    118: "18 T20 D20",
+    119: "19 T20 D20",
+    120: "T20 20 D20",
+    121: "T19 D19 D13",
+    122: "D20 T20 D11",
+    123: "T19 D19 D14",
+    124: "D20 T20 D12",
+    125: "D15 T19 D19",
+    126: "T20 D19 D14",
+    127: "D15 T19 D20",
+    128: "D15 T20 D19",
+    129: "D17 T19 D19",
+    130: "T20 D15 D20",
+    131: "T19 D17 D20",
+    132: "T20 D17 D19",
+    133: "D18 T19 D20",
+    134: "D18 T20 D19",
+    135: "T19 D20 D19",
+    136: "D18 T20 D20",
+    137: "T19 D20 D20",
+    138: "T20 D19 D20",
+    139: "T19 T14 D20",
+    140: "T20 D20 D20",
+    141: "T19 T20 D12",
+    142: "T20 T14 D20",
+    143: "T15 T20 D19",
+    144: "T20 T18 D15",
+    145: "T15 T20 D20",
+    146: "T16 T20 D19",
+    147: "T20 T19 D15",
+    148: "T20 T16 D20",
+    149: "T20 T19 D16",
+    150: "T20 T18 D18",
+    151: "T20 T19 D17",
+    152: "T20 T18 D19",
+    153: "T20 T19 D18",
+    154: "T20 T18 D20",
+    155: "T20 T19 D19",
+    156: "T20 T20 D18",
+    157: "T20 T19 D20",
+    158: "T20 T20 D19",
+    159: "No checkout",
     160: "T20 T20 D20",
-
-    136: "T20 T20 D8",
-
-    36: "D18"
+    161: "T20 T17 Bull",
+    162: "No checkout",
+    163: "No checkout",
+    164: "T20 T18 Bull",
+    165: "No checkout",
+    166: "No checkout",
+    167: "T20 T19 Bull",
+    168: "No checkout",
+    169: "No checkout",
+    170: "T20 T20 Bull"
 }
 
-STARTING_TOTAL = 501
-
+# STARTING_TOTAL = 501
 
 class X01Match(MatchManager, MatchVisitTemplate):
 
-    def __init__(self):
+    def __init__(self, starting_total):
         super().__init__()
+        self._starting_total = starting_total
         self.scores = []  # list of scores remaining parallel to players
         self.averages = []  # single-dart average (x 3 for 3-dart average)
         self.first9 = []  # average for first 9 darts
@@ -28,7 +200,7 @@ class X01Match(MatchManager, MatchVisitTemplate):
     # This has the potential to be buggy if the match is set first and players registered after
     def post_init(self):
         for i in range(0, len(self.match.players)):
-            self.scores.append(STARTING_TOTAL)  # Might want to parameterize the starting total
+            self.scores.append(self._starting_total)  # Might want to parameterize the starting total
             self.first9.append(None)
             self.averages.append(None)
 
@@ -86,7 +258,7 @@ class X01Match(MatchManager, MatchVisitTemplate):
         if len(self.match.visits[player_index]) == 3:
             # subtract the remaining score from starting score and / 3 to get average?
             # check logic, why /  3?
-            self.first9[player_index] = (STARTING_TOTAL - self.scores[player_index]) / 3
+            self.first9[player_index] = (self._starting_total - self.scores[player_index]) / 3
 
         # Calculate single-dart average taking account of a double being hit with dart 1 or 2 when checking out
         # player threw 3 darts per visit unless on the winning one
@@ -99,7 +271,7 @@ class X01Match(MatchManager, MatchVisitTemplate):
             self.match.winning_player_index = player_index
 
         # set averages for player
-        self.averages[player_index] = (STARTING_TOTAL - self.scores[player_index]) / num_darts_thrown
+        self.averages[player_index] = (self._starting_total - self.scores[player_index]) / num_darts_thrown
 
     def format_summary(self, player_index, visit):
         # Include suggested checkout if remaining score can be checked out in 3 darts
@@ -133,5 +305,5 @@ class X01MatchBuilder:
     def __init__(self):
         pass
 
-    def __call__(self):
-        return X01Match()
+    def __call__(self, starting_total):
+        return X01Match(starting_total)
--- app/matches_301.py
+++ app/matches_301.py
@@ -9,50 +9,63 @@ factory = object_factory.ObjectFactory()
 # register the builder for X01 game type
 factory.register_builder('X01', x01_match.X01MatchBuilder())
 
+config = {
+    'starting_total': 301
+}
+
 # get a reference to the factory
-x01 = factory.create('X01')
+x01 = factory.create('X01', **config)
 # create a darts match
 match = darts_match.DartsMatch()
 
 # create players in the match
-player1_index = match.register_player('Alice')
-player2_index = match.register_player('Kalifa')
+player1_index = match.register_player('Yellow')
+player2_index = match.register_player('Blue')
 
 x01.set_match(match)
 
-my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.TREBLE, 20), (DartMultiplier.SINGLE, 5)])
+# 30 - Yellow
+my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.SINGLE, 5), (DartMultiplier.SINGLE, 5)])
 result, response = x01.process_visit(player1_index, my_visit)
 print(response)
 
+# 26 - Blue
 my_visit = visit.Visit([(DartMultiplier.SINGLE, 1), (DartMultiplier.SINGLE, 5), (DartMultiplier.SINGLE, 20)])
 result, response = x01.process_visit(player2_index, my_visit)
 print(response)
 
-my_visit = visit.Visit([(DartMultiplier.TREBLE, 20), (DartMultiplier.TREBLE, 20), (DartMultiplier.TREBLE, 20)])
+# 135 - Yellow
+my_visit = visit.Visit([(DartMultiplier.TREBLE, 20), (DartMultiplier.TREBLE, 20), (DartMultiplier.TREBLE, 5)])
 result, response = x01.process_visit(player1_index, my_visit)
 print(response)
 
-my_visit = visit.Visit([(DartMultiplier.TREBLE, 5), (DartMultiplier.SINGLE, 20), (DartMultiplier.SINGLE, 20)])
+# 46 - Blue
+my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.SINGLE, 19), (DartMultiplier.SINGLE, 20)])
 result, response = x01.process_visit(player2_index, my_visit)
 print(response)
 
-my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.TREBLE, 20), (DartMultiplier.SINGLE, 20)])
+# 81 - Yellow
+my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.TREBLE, 20), (DartMultiplier.SINGLE, 1)])
 result, response = x01.process_visit(player1_index, my_visit)
 print(response)
 
-my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.TREBLE, 20), (DartMultiplier.SINGLE, 20)])
+# 123 - Blue
+my_visit = visit.Visit([(DartMultiplier.TREBLE, 20), (DartMultiplier.TREBLE, 20), (DartMultiplier.TREBLE, 3)])
 result, response = x01.process_visit(player2_index, my_visit)
 print(response)
 
-my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.SINGLE, 20), (DartMultiplier.TREBLE, 20)])
+# 30 - Yellow
+my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.SINGLE, 10), (DartMultiplier.MISS, 0)])
 result, response = x01.process_visit(player1_index, my_visit)
 print(response)
 
+# 58 - Blue
 my_visit = visit.Visit([(DartMultiplier.SINGLE, 20), (DartMultiplier.SINGLE, 20), (DartMultiplier.SINGLE, 1)])
 result, response = x01.process_visit(player2_index, my_visit)
 print(response)
 
-my_visit = visit.Visit([(DartMultiplier.SINGLE, 0), (DartMultiplier.DOUBLE, 18), (DartMultiplier.SINGLE, 0)])
+# 25 - Yellow
+my_visit = visit.Visit([(DartMultiplier.SINGLE, 9), (DartMultiplier.DOUBLE, 8), (DartMultiplier.SINGLE, 0)])
 result, response = x01.process_visit(player1_index, my_visit)
 print(response)
 
