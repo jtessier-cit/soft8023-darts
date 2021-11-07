1d58338f1feb8f668f1c51974139be4cebed15d5
Jason Tessier <jason.tessier@gmail.com>
2021-11-07 13:05:29 +0000

    Added some comments, ran with/without threads and singleton.

--- dao/darts_match_dao_thread_safe_singleton.py
+++ dao/darts_match_dao_thread_safe_singleton.py
@@ -8,18 +8,26 @@ import time
 class DartsMatchDao:
     __instance = None
 
+    # factory method to create an instance if there is none
     @staticmethod
     def get_instance():
+        # check if there is an instance
         if DartsMatchDao.__instance is None:
+            # using locking
             with threading.Lock():
+                # check again if there is an instance
                 if DartsMatchDao.__instance is None:  # Double locking mechanism
+                    # create only if there is no instance
                     DartsMatchDao()
+        # return the instance
         return DartsMatchDao.__instance
 
     def __init__(self):
+        # should never be creating an instance if there is one
         if DartsMatchDao.__instance is not None:
             raise Exception("This is a singleton!")
         else:
+            # set the class/instance variable __instance to the new object
             DartsMatchDao.__instance = self
         self.db = TinyDB('db.json')
         self.lock = threading.Lock()
@@ -27,7 +35,7 @@ class DartsMatchDao:
 
     def add(self, match):
         self.lock.acquire()
-
+        print("locked in add")
         time.sleep(4)
 
         Match = Query()
@@ -37,3 +45,4 @@ class DartsMatchDao:
         print('Insert attempted on ' + match.player1 + '    ' + str(self.rand))
 
         self.lock.release()
+        print("unlocked in add")
--- test_threads.py
+++ test_threads.py
@@ -1,4 +1,4 @@
-from dao import darts_match_dao_thread_safe_singleton
+from dao import darts_match_dao_thread_safe_singleton, darts_match_dao
 from domain import darts_match
 
 import threading
@@ -11,6 +11,7 @@ class AddThread(threading.Thread):
         self.name = name
         self.match = match
         self.dao = darts_match_dao_thread_safe_singleton.DartsMatchDao.get_instance()
+        # self.dao = darts_match_dao.DartsMatchDao()
 
     def run(self):
         print("Starting " + self.name)
@@ -22,7 +23,7 @@ dart_match1 = darts_match.DartsMatch('501', 'Dupe', 'Dup2')
 thread1 = AddThread(1, "Thread-1", dart_match1)
 
 dart_match2 = darts_match.DartsMatch('501', 'Dupe', 'Dup2')
-thread2 = AddThread(2, "Thread-2", dart_match2)
+thread2 = AddThread(2, "Thread-2",   dart_match2)
 
 thread1.start()
 thread2.start()
